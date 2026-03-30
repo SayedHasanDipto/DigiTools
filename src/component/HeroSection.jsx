03ca222649d030aef0d0eee464afebf4ba1dbd29
Sayed Hasan Dipto <sayedhasandipto@gmail.com>
2026-03-31 00:25:21 +0600

    responsive for mobile devices

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -8,20 +8,20 @@ import { CiPlay1 } from "react-icons/ci";
 
 const HeroSection = () => {
     return (
-        <div className="hero max-w-7xl mx-auto flex justify-between items-center min-h-screen manrope">
+        <div className="hero max-w-7xl mx-auto flex justify-between items-center min-h-0 lg:min-h-screen h-auto py-20 lg:py-0 manrope">
             <div>
-                <button className='mb-2 py-0 px-0 rounded-full animate-bounce'>
+                <button className='mb-2 py-0 px-0 rounded-full animate-bounce max-sm:ml-8'>
                     <div className='flex items-center justify-center gap-2 py-2 px-4 rounded-full bg-[#E1E7FF]'>
                         <img src={circleImg} alt="circleImg" className='animate-pulse' />
                         <h1 className='color font-medium'>New: AI-Powered Tools Available</h1>
                     </div>
                 </button>
-                <h1 className='text-7xl font-extrabold text-[#101727] w-2xl leading-20 mb-4'>Supercharge Your Digital Workflow</h1>
-                <p className='text-lg w-lg leading-8'>
+                <h1 className='text-7xl max-sm:text-xl max-sm:text-[3rem] max-sm:w-[25rem] max-sm:text-center font-extrabold text-[#101727] w-2xl leading-20 mb-4'>Supercharge Your Digital Workflow</h1>
+                <p className='text-lg w-lg leading-8 max-sm:w-[22rem] max-sm:mx-auto max-sm:text-lg'>
                     Access premium AI tools, design assets, templates, and productivity
                     software—all in one place. Start creating faster today. Explore Products
                 </p>
-                <div className='mt-8 gap-4 flex items-center'>
+                <div className='mt-8 gap-4 flex items-center max-sm:justify-center'>
                     <button className='font-semibold text-white font-lg btn rounded-full btn-secondary h-[3rem]'>Explore Products</button>
                     <button className='font-semibold font-lg h-[3rem] btn btn-secondary btn-outline rounded-full hover:text-white flex gap-2 items-center'><CiPlay1 className='scale-125' /> Watch Demo</button>
                 </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,14 +5,14 @@ import { FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
     return (
-        <div className='border-b border-gray-200 shadow manrope'>
+        <div className='border-b border-gray-200 shadow-md manrope'>
             <div className='max-w-7xl mx-auto my-2'>
                 <div className="navbar">
                     <div className="navbar-start">
                         <a href="" className='text-3xl font-bold text-[#f43098]'>DigiTools</a>
                     </div>
                     <div className="navbar-center">
-                        <ul className='flex items-center justify-center gap-8 text-lg'>
+                        <ul className='hidden md:flex items-center justify-center gap-8 text-lg'>
                             <li><a href="/Products" className='font-semibold btn btn-secondary text-black hover:text-white btn-ghost'>Products</a></li>
                             <li><a href="Features" className='font-semibold btn btn-secondary text-black hover:text-white btn-ghost'>Features</a></li>
                             <li><a href="/Pricing" className='font-semibold btn btn-secondary text-black hover:text-white btn-ghost'>Pricing</a></li>
@@ -20,8 +20,8 @@ const Navbar = () => {
                             <li><a href="/FAQ" className='font-semibold btn btn-secondary text-black hover:text-white btn-ghost'>FAQ</a></li>
                         </ul>
                     </div>
-                    <div className="navbar-end gap-4">
-                        <button className='hover:bg-[#f43098] duration-150 ease-in hover:text-white w-8 scale-125 flex items-center justify-center rounded-full h-8'><FiShoppingCart /></button>
+                    <div className="navbar-end gap-4 max-sm:gap-4">
+                        <button className='hover:bg-[#f43098] max-sm:hover:bg-white max-sm:hover:text-black duration-150 ease-in hover:text-white w-8 scale-125 flex items-center justify-center rounded-full h-8'><FiShoppingCart /></button>
                         <button className='font-semibold font-lg btn btn-secondary text-black hover:text-white btn-ghost rounded-full h-[2.5rem]'>Login</button>
                         <button className='font-semibold text-white font-lg btn rounded-full btn-secondary h-[2.5rem]'>Get Started</button>
                     </div>
